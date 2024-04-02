3bf40d57a0504ac21a60489aab8bc3830ced9427
Ruan Bindaco <ruanazsxdc123@gmail.com>
2024-04-02 15:08:28 -0300

    Creating cia-lineup-2024 project

---
+++ src/views/Home.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="login-page">
+    <div class="header">
+      <h1 class="title">CIA 2024</h1>
+      <p class="subtitle">Gerador de Line-up</p>
+    </div>
+    <div class="text">
+      Descrubra sua line perfeita de acordo com seus artistas favoritos
+    </div>
+    <div @click="login" class="login-button">Entre com o seu spotify</div>
+
+    <div class="actual-cia">
+      <p class="cia-title">Line Original</p>
+      <img
+        class="img-cia"
+        src="../assets/images/line-original.jpg"
+        alt="Line Original"
+      />
+      <p>
+        Não conhece a CIA? Acesse o Instagram deles clicando
+        <a href="https://www.instagram.com/copainteratleticas/">aqui.</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    login() {
+      console.log('login')
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.login-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 70px;
+  padding: 25px 32px;
+
+  .header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .title {
+      font-size: 55px;
+    }
+    .subtitle {
+      font-size: 35px;
+    }
+  }
+  .text {
+    font-size: 28px;
+  }
+  .login-button {
+    font-size: 25px;
+    padding: 21px 15px;
+    border-radius: 10px;
+    border: 1px solid #ccc;
+    background-color: #005355;
+    color: #fff;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #08838e;
+    }
+  }
+
+  .actual-cia {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+    padding-top: 32px;
+    .cia-title {
+      font-size: 40px;
+    }
+    .img-cia {
+      max-width: 600px;
+      width: 100%;
+    }
+  }
+}
+</style>
